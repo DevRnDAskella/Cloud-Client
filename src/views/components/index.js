c0b9cb7eda97265e3f82de721cdbcca1569c596f
RnD.Askella <dev_rnd.askella@yahoo.com>
2022-06-21 21:35:52 +0300

    Add index files for structures

---
+++ src/views/components/index.js
@@ -0,0 +1,4 @@
+export { Navbar } from "./navbar/Navbar";
+export { Input } from "./input/Input";
+export { default as Registration } from "./registration/Registration";
+export { default as Login } from "./login/Login";
